d1d6e4deed21d7c5f5f7af5c714fe38257499a08
August <auggierutter@gmail.com>
2021-12-16 15:40:28 -0500

    commit tree 4813d601dccd0442e1c127122a5beea90c76ed99

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,16 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     items = Item.all
     render json: items, include: :user
   end
-
+  def show
+  items = Item.find(params[:id])
+  render json: items, include: :user 
+  end
+private
+def render_not_found_response
+  render json: { error: " not found" }, status: :not_found
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
+
   def show
+    if params[:user_id]
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+    else render json:{ error: "not found"}, status: :not_found
   end
+  end
+ 
 
+ 
 end
